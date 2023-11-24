2554f36be56b318c2807361526e357a23d684f61
Bogdan <catiiiiii@gmail.com>
2023-11-24 18:41:55 +0500

    settings to walking by crossroads

--- __init__.py
+++ __init__.py
@@ -12,6 +12,7 @@ maze[len(maze) - 2][len(maze) - 2] = 'E'
 # Start position
 maze[1][1] = '@'
 
+crossroads = []
 
 def step():
     for i in range(0, len(maze)):
@@ -48,7 +49,7 @@ def step():
                             maze[i][maze[i].index('@') - 1] = '@'
                             maze[i][maze[i].index('@') + 1] = 'X'
                 if len(func) > 1:
-                    crossroads = (i, maze[i].index('@'))
+                    crossroads.append([i, maze[i].index('@')])
                     match func[0]:
                         case 'UP':
                             maze[i - 1][maze[i].index('@')] = '@'
@@ -62,13 +63,17 @@ def step():
                         case 'LEFT':
                             maze[i][maze[i].index('@') - 1] = '@'
                             maze[i][maze[i].index('@') + 1] = 'X'
-                    print(crossroads)
+                if func[0] == 'END':
+                    maze[i][maze[i].index('@')] = 'X'
+                    maze[crossroads[0][0]][crossroads[0][1]] = '@'
+                    del crossroads[0]
+                print(crossroads)
 
             make_step(check_step())
             break
 
 
-for i in range(1, 24):
+for i in range(1, 635):
     step()
 
 for i in maze:
