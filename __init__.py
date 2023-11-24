234fbccf1bf04f13ac49a6ddedd286d653545d12
Bogdan <catiiiiii@gmail.com>
2023-11-24 18:51:32 +0500

    settings to find end of the maze

--- __init__.py
+++ __init__.py
@@ -21,15 +21,22 @@ def step():
                 variants = []
                 if maze[i - 1][maze[i].index('@')] == '0':  # if it can go UP
                     variants.append('UP')
+                elif maze[i - 1][maze[i].index('@')] == 'E':
+                    variants.append('EXIT')
                 if maze[i + 1][maze[i].index('@')] == '0':  # if it can go DOWN
                     variants.append('DOWN')
+                elif maze[i + 1][maze[i].index('@')] == 'E':
+                    variants.append('EXIT')
                 if maze[i][maze[i].index('@') + 1] == '0':  # if it can go RIGHT
                     variants.append('RIGHT')
+                elif maze[i][maze[i].index('@') + 1] == 'E':
+                    variants.append('EXIT')
                 if maze[i][maze[i].index('@') - 1] == '0':  # if it can go LEFT
                     variants.append('LEFT')
+                elif maze[i][maze[i].index('@') - 1] == 'E':
+                    variants.append('EXIT')
                 if len(variants) == 0:
                     variants = ['END']
-
                 return variants
 
             def make_step(func: list):
@@ -67,13 +74,16 @@ def step():
                     maze[i][maze[i].index('@')] = 'X'
                     maze[crossroads[0][0]][crossroads[0][1]] = '@'
                     del crossroads[0]
+                if func[0] == 'EXIT':
+                    maze[i][maze[i].index('@')] = 'X'
+                    maze[len(maze) - 2][len(maze) - 2] = '@'
                 print(crossroads)
 
             make_step(check_step())
             break
 
 
-for i in range(1, 635):
+for i in range(1, 638):
     step()
 
 for i in maze:
